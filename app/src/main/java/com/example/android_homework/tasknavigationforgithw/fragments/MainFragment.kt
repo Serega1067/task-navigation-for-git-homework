8646cb5b791d1bb650aa1d6ce050a04c5e72f3df
Serega1067 <serega1067@yandex.ru>
2025-03-16 03:44:30 +0700

    Added import to MainFragment and added layout fragment_main

--- app/src/main/java/com/example/android_homework/tasknavigationforgithw/fragments/MainFragment.kt
+++ app/src/main/java/com/example/android_homework/tasknavigationforgithw/fragments/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.android_homework.tasknavigationforgithw.R
+import android.widget.Button
 
 class MainFragment : Fragment() {
 
